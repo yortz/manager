0349f2cdb7fcd3488b73e7ce0557dafcf6343ac6
yortz <skylarking420@gmail.com>
2013-12-06 11:41:24 +0100

    Speccing companies page.

--- spec/company_spec.rb
+++ spec/company_spec.rb
@@ -2,19 +2,28 @@ require 'spec_helper'
 
 describe "Api::v1::Company" do
   describe "GET /companies" do
-    before          { get "/api/v1/companies" }
+
     COMPANIES = [ {address: "via le dita dal naso 6", country: "Italy", city: "Milan"},
                   {address: "Broad Street 87", country: "Usa", city: "Richmond"},
                   {address: "M Street 15", country: "Usa", city: "Washington D.C." } ]
-    let(:json)      { json_parse(last_response.body) }
-    let(:companies) { create_companies(COMPANIES) } #json[:companies] }
+
+    before do
+      get "/api/v1/companies"
+      create_companies(COMPANIES)
+    end
+
+    let(:companies) do
+      companies = Company.all
+      companies.map { |company| json[:company] }
+    end
+    let(:json)  { json_parse(last_response.body) }
 
     it "responds successfully" do
       last_response.status.should be 200
       json[:status].should == "success"
     end
 
-    it "returns 3 users" do
+    it "returns 3 companies" do
       companies.size.should equal 3
     end
   end
