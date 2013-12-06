9e7c39ed07de271551c8fd2126a5730072a75c97
yortz <skylarking420@gmail.com>
2013-12-06 12:56:19 +0100

    Adding helpers to main app file and dynamically setting db from the values in the yml file.

--- app.rb
+++ app.rb
@@ -5,7 +5,27 @@ require 'carrierwave'
 require 'carrierwave/orm/activerecord'
 require "sinatra/activerecord"
 
-set :database, {adapter: "mysql", database: "manager"}
+config = YAML::load(File.open(File.dirname(__FILE__) + '/db/database.yml'))
+set :database, {adapter: config["adapter"], database: config["database"]}
+
+# Helpers
+module JsonHelpers
+  def json(hash)
+    MultiJson.dump(hash, pretty: true)
+  end
+
+  def parsed_params
+    if request.get? || request.form_data?
+      parsed = params
+    else
+      parsed = MultiJson.load(request.body, symbolize_keys: true)
+    end
+
+    parsed = {} unless parsed.is_a?(Hash)
+
+    return parsed
+  end
+end
 
 class PassportUploader < CarrierWave::Uploader::Base
   def store_dir
@@ -46,11 +66,25 @@ class App < Sinatra::Base
     end
   end
 
+  helpers JsonHelpers
+
   namespace "/api/v1" do
     get "/companies" do
       companies = Company.all
       json({ status: "success", companies: companies })
     end
+
+    post "/companies" do
+      company = parsed_params[:company]
+      c = Company.new(company)
+
+      if c.save
+        json({ status: "success", company: company })
+      else
+        halt 500
+      end
+    end
   end
+
 
 end
